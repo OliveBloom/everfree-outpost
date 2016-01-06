5c804d466c4cc213dd866012fec238f02c6532a1
Stuart Pernsteiner <stuart@pernsteiner.org>
2016-01-06 09:04:24 -0800

    convert all walls and doors to python

--- scripts2/outpost/structure_items.py
+++ scripts2/outpost/structure_items.py
@@ -6,7 +6,22 @@ mallet.register('road/', mallet.TERRAIN_VARIANTS)
 autorotate.register_floor('house_floor', 'wood_floor')
 mallet.register('wood_floor/', mallet.TERRAIN_VARIANTS)
 
-autorotate.register_wall('interior_wall')
-mallet.register('interior_wall/', mallet.WALL_VARIANTS)
-structure_items.register('interior_door', 'interior_wall/door/closed', 'axe')
-door.register('interior_wall/door', tool_name='axe')
+def wall_and_door(name, tool, extra_variants=()):
+    autorotate.register_wall('%s_wall' % name)
+    mallet.register('%s_wall/' % name,
+            ('edge/horiz',) + extra_variants + mallet.COMMON_WALL_VARIANTS)
+    structure_items.register('%s_door' % name, '%s_wall/door/closed' % name, tool)
+    door.register('%s_wall/door' % name, tool_name=tool)
+
+wall_and_door('interior', 'axe')
+wall_and_door('brick', 'pickaxe')
+wall_and_door('wood', 'axe', ('window/v0',))
+wall_and_door('stone', 'pickaxe', ('window/v0', 'window/v1',))
+wall_and_door('cottage', 'axe',
+        ('variant/v0', 'variant/v1', 'variant/v2', 'window/v0', 'window/v1',))
+
+# ruined_wall doesn't have a proper door, so we can't use wall_and_door
+autorotate.register_wall('ruined_wall')
+mallet.register('ruined_wall/',
+        ('edge/horiz', 'window/v0', 'window/v1',) + mallet.WALL_VARIANTS)
+structure_items.register('ruined_door', 'ruined_wall/door/open', 'pickaxe')
