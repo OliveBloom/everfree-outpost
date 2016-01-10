6ec9e36f92a8cf7032caeaeacbc6a7333502d733
Stuart Pernsteiner <stuart@pernsteiner.org>
2016-01-09 18:47:41 -0800

    add forest_check for mining walls

--- scripts2/outpost/cave.py
+++ scripts2/outpost/cave.py
@@ -1,7 +1,6 @@
 from outpost_server.core import util
 from outpost_server.core.data import DATA
-from outpost_server.outpost.lib import tool
-from outpost_server.outpost.lib import ward
+from outpost_server.outpost.lib import tool, util as util2, ward
 
 @tool.handler('pickaxe', 'cave_junk/0')
 @tool.handler('pickaxe', 'cave_junk/1')
@@ -12,9 +11,9 @@ def cave_junk(e, s, args):
     e.inv('main').bulk_add(DATA.item('stone'), 2)
 
 def mine_wall(e, args):
-    # TODO: check forest
     pos = util.hit_tile(e)
     ward.check(e, pos)
+    util2.forest_check(e)
 
     if e.plane().set_cave(pos):
         e.inv('main').bulk_remove(DATA.item('pick'), 1)
