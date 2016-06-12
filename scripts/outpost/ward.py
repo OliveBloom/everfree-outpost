71f96504a419ad9ae4f7e402267a9bc269731edd
Stuart Pernsteiner <stuart@pernsteiner.org>
2016-06-12 15:49:53 -0700

    prevent guests from placing wards

--- scripts/outpost/ward.py
+++ scripts/outpost/ward.py
@@ -11,8 +11,14 @@ TEMPLATE = DATA.template('ward')
 @use.item(ITEM)
 def use_item(e, args):
     util2.forest_check(e)
-    pos = util.hit_tile(e)
 
+    # TODO: hacky check for guest account status
+    if ':' in e.controller().name():
+        e.controller().send_message(
+            'You must register an account before placing wards.')
+        return
+
+    pos = util.hit_tile(e)
     ok, msg = ward.can_add(e, pos)
     if not ok:
         e.controller().send_message(msg)
