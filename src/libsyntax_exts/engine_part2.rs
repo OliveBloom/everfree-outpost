da737ac822119040380ffb52704309474bd13462
Stuart Pernsteiner <stuart@pernsteiner.org>
2016-06-14 08:07:15 -0700

    new engine-split implementation

---
+++ src/libsyntax_exts/engine_part2.rs
@@ -0,0 +1,201 @@
+use std::collections::HashMap;
+use std::error::Error;
+
+use syntax::ast::TokenTree;
+use syntax::codemap::Span;
+use syntax::ext::base::{ExtCtxt, MacResult, MacEager, DummyResult};
+use syntax::parse;
+use syntax::parse::token::{Token, DelimToken};
+use syntax::util::small_vector::SmallVector;
+
+use parser::{self, Parser};
+
+use self::parts::*;
+
+mod parts {
+    #![allow(dead_code)]
+
+    macro_rules! parts {
+        ($flags_name:ident :
+                $(($code:ident $idx:expr) $name:ident = $ty:ty;)*) => {
+            bitflags! {
+                pub flags $flags_name: u32 {
+                    $(
+                        const $name = 1 << $idx,
+                    )*
+                }
+            }
+
+            pub static PART_NAMES: &'static [&'static str] = &[
+                $(stringify!($name),)*
+            ];
+
+            pub static PART_TYPES: &'static [&'static str] = &[
+                $(stringify!($ty),)*
+            ];
+
+            pub static PART_CODES: &'static [&'static str] = &[
+                $(stringify!($code),)*
+            ];
+        };
+    }
+
+    parts! {
+        EngineParts:
+            (Wr  0) WORLD =         ::world::World<'d>;
+            (Ex  1) EXTRA =         ::logic::extra::Extra;
+            (Ms  2) MESSAGES =      ::messages::Messages;
+            (Ti  3) TIMER =         ::timer::Timer;
+            (Ph  4) PHYSICS =       ::physics::Physics<'d>;
+            (Vi  5) VISION =        ::vision::Vision;
+            (Ch  6) CHUNKS =        ::chunks::Chunks<'d>;
+            (Ca  7) CACHE =         ::cache::TerrainCache;
+            (Tg  8) TERRAIN_GEN =   ::terrain_gen::TerrainGen;
+            (Ct  9) CHAT =          ::chat::Chat;
+            (In 10) INPUT =         ::input::Input;
+    }
+}
+
+const NUM_PARTS: usize = 11;
+
+fn build_flag_map() -> HashMap<&'static str, EngineParts> {
+    #![allow(non_snake_case, unused_variables)]
+    let mut fm = HashMap::new();
+    macro_rules! flags {
+        ($($name:ident = $value:expr;)*) => {
+            {
+                $(
+                    let $name = {
+                        fm.insert(stringify!($name), $value);
+                        $value
+                    };
+                )*
+            }
+        };
+    }
+
+    flags!(
+        world = WORLD;
+        extra = EXTRA;
+        messages = MESSAGES;
+        timer = TIMER;
+        physics = PHYSICS;
+        vision = VISION;
+        chunks = CHUNKS;
+        cache = CACHE;
+        terrain_gen = TERRAIN_GEN;
+        chat = CHAT;
+        input = INPUT;
+
+        all = EngineParts::all();
+
+        // Example syntax:
+        //VisionHooks = world | messages;
+    );
+
+    fm
+}
+
+fn parse_engine_part2<'a>(table: &HashMap<&'static str, EngineParts>,
+                          mut p: Parser<'a>)
+                          -> parser::Result<(bool, String, EngineParts)> {
+    let is_pub = p.take_word("pub").is_ok();
+    let (name, name_sp) = try!(p.take_ident());
+
+    // Support shorthand syntax: engine_part_typedef!(pub VisionHooks);
+    // Meaning: engine_part_typedef!(pub VisionHooks(VisionHooks));
+    let mut body = match p.take_delimited(DelimToken::Paren) {
+        Ok(x) => x,
+        Err(_) => {
+            if !p.eof() {
+                return p.expected("open paren or end of input");
+            }
+            if let Some(&flags) = table.get(&name as &str) {
+                return Ok((is_pub, name, flags));
+            } else {
+                fail!(name_sp, "struct name {:?} is not a valid engine part name", name);
+            }
+        }
+    };
+
+    let mut flags = EngineParts::empty();
+
+    // Whether it's okay to accept an ident in the current state.
+    let mut ready = true;
+    while !body.eof() {
+        if !ready {
+            return body.expected("comma");
+        }
+        ready = false;
+
+        let (part_name, part_name_sp) = try!(body.take_ident());
+        if let Some(&part_flags) = table.get(&part_name as &str) {
+            flags = flags | part_flags;
+        } else {
+            fail!(part_name_sp, "expected engine part name, but saw {:?}", part_name);
+        }
+
+        // Try to take a comma.  On success, we can take another ident.  In either case, we can
+        // take the closing paren.
+        if let Ok(_) = body.take_exact(Token::Comma) {
+            ready = true;
+        }
+    }
+
+    try!(body.take_eof());
+
+    Ok((is_pub, name, flags))
+}
+
+pub fn engine_part2(cx: &mut ExtCtxt,
+                    sp: Span,
+                    args: &[TokenTree]) -> Box<MacResult + 'static> {
+    let fm = build_flag_map();
+    let p = Parser::new(sp, args);
+    let (is_pub, name, flags) = match parse_engine_part2(&fm, p) {
+        Ok(x) => x,
+        Err(e) => {
+            cx.span_err(e.1, e.description());
+            return DummyResult::any(sp);
+        },
+    };
+
+    let mut code = String::new();
+
+    code.push_str(&format!("{} struct {}<'d> {{\n", if is_pub { "pub" } else { "" }, name));
+    code.push_str("    _data: &'d ::data::Data,\n");
+    code.push_str("    _storage: &'d ::storage::Storage,\n");
+    code.push_str("    _script_hooks: &'d ::script::ScriptHooks,\n");
+    code.push_str("    _now: ::types::Time,\n");
+
+    for i in 0 .. NUM_PARTS {
+        let field_name = PART_NAMES[i].to_lowercase();
+        let ty = PART_TYPES[i];
+
+        if flags.bits() & (1 << i) == 0 {
+            code.push_str(&format!("    _{}: {},\n", field_name, ty));
+        } else {
+            code.push_str(&format!("    pub {}: {},\n", field_name, ty));
+        }
+    }
+
+    code.push_str("}\n");
+
+
+    let mut items = SmallVector::zero();
+
+    // TODO: error handling
+    items.push(parse::parse_item_from_source_str(
+            "<engine_part2!>".to_owned(),
+            code,
+            cx.cfg.clone(),
+            cx.parse_sess).unwrap());
+    items.push(parse::parse_item_from_source_str(
+            "<engine_part2!>".to_owned(),
+            format!("engine_part2_impl!({});", name),
+            cx.cfg.clone(),
+            cx.parse_sess).unwrap());
+
+    MacEager::items(items)
+}
+
---
+++ src/server/engine/split2.rs
@@ -0,0 +1,171 @@
+use std::marker::PhantomData;
+use std::mem;
+
+use types::*;
+
+use data::Data;
+use engine::Engine;
+use storage::Storage;
+use script::ScriptHooks;
+
+
+pub struct Static<'d> {
+    pub data: &'d Data,
+    pub storage: &'d Storage,
+    pub script_hooks: &'d ScriptHooks,
+    pub now: Time,
+}
+
+// The general scheme of the generated structs looks like this:
+//
+//  pub Engine<'d> {
+//      _data: &'d Data,
+//      _storage: &'d Storage,
+//      _script_hooks: &'d ScriptHooks,
+//      _now: Time,
+//
+//      pub world: World<'d>,
+//
+//      pub extra: Extra,
+//      pub messages: Messages,
+//      _timer: Timer,
+//      pub physics: Physics<'d>,
+//      _vision: Vision,
+//      _chunks: Chunks<'d>,
+//      pub cache: TerrainCache,
+//      pub terrain_gen: TerrainGen,
+//      _chat: Chat,
+//
+//      pub input: Input,
+//  }
+//
+// "Available" fields are `pub`, the rest are non-pub and prefixed with an underscore (so that the
+// error appears during an earlier compilation pass).  The static fields (data .. now) are all
+// private, so that they can't be written by either half after a split.
+
+macro_rules! engine_part2_impl {
+    ($name:ident) => {
+        impl<'d> $name<'d> {
+            pub fn data(&self) -> &'d ::data::Data {
+                self._data
+            }
+
+            pub fn storage(&self) -> &'d ::storage::Storage {
+                self._storage
+            }
+
+            pub fn script_hooks(&self) -> &'d ::script::ScriptHooks {
+                self._script_hooks
+            }
+
+            pub fn now(&self) -> ::types::Time {
+                self._now
+            }
+
+            pub fn open_static(&self) -> ::engine::split2::Static<'d> {
+                Static {
+                    data: self._data,
+                    storage: self._storage,
+                    script_hooks: self._script_hooks,
+                    now: self._now,
+                }
+            }
+        }
+    };
+}
+
+
+
+pub struct Y<T>(PhantomData<T>);
+pub struct N<T>(PhantomData<T>);
+pub struct E;
+
+
+pub trait BitList {
+    fn code() -> u64;
+    fn len() -> usize;
+}
+
+impl<T: BitList> BitList for Y<T> {
+    fn code() -> u64 {
+        (<T as BitList>::code() << 1) | 1
+    }
+
+    fn len() -> usize {
+        <T as BitList>::len() + 1
+    }
+}
+
+impl<T: BitList> BitList for N<T> {
+    fn code() -> u64 {
+        (<T as BitList>::code() << 1) | 0
+    }
+
+    fn len() -> usize {
+        <T as BitList>::len() + 1
+    }
+}
+
+impl BitList for E {
+    fn code() -> u64 {
+        0
+    }
+
+    fn len() -> usize {
+        0
+    }
+}
+
+
+pub unsafe trait RefineTo<Target: BitList>: BitList {
+    type Remnant: BitList;
+}
+unsafe impl<A, B: BitList> RefineTo<Y<B>> for Y<A> where A: RefineTo<B> {
+    type Remnant = N<<A as RefineTo<B>>::Remnant>;
+}
+unsafe impl<A, B: BitList> RefineTo<N<B>> for Y<A> where A: RefineTo<B> {
+    type Remnant = Y<<A as RefineTo<B>>::Remnant>;
+}
+unsafe impl<A, B: BitList> RefineTo<N<B>> for N<A> where A: RefineTo<B> {
+    type Remnant = N<<A as RefineTo<B>>::Remnant>;
+}
+unsafe impl RefineTo<E> for E {
+    type Remnant = E;
+}
+
+
+pub unsafe trait Coded: Sized {
+    type Code: BitList;
+
+    fn refine<'a, Target: Coded>(&'a mut self) -> &'a mut Target
+            where Self::Code: RefineTo<Target::Code> {
+        unsafe { mem::transmute(self) }
+    }
+
+    fn split<'a,
+             Target: Coded,
+             Rest: Coded<Code=<Self::Code as RefineTo<Target::Code>>::Remnant>
+            >(&'a mut self) -> (&'a mut Target, &'a mut Rest)
+            where Self::Code: RefineTo<Target::Code> {
+        let ptr = self as *mut Self;
+        unsafe { (mem::transmute(ptr), mem::transmute(ptr)) }
+    }
+
+    fn refine_dynamic<'a, Target: Coded>(&'a mut self) -> Option<&'a mut Target> {
+        if <Self::Code as BitList>::len() != <Target::Code as BitList>::len() {
+            return None;
+        }
+
+        let self_code = <Self::Code as BitList>::code();
+        let target_code = <Target::Code as BitList>::code();
+        if self_code & target_code == target_code {
+            Some(unsafe { mem::transmute(self) })
+        } else {
+            None
+        }
+    }
+}
+
+unsafe impl<'d> Coded for Engine<'d> {
+    type Code = Y<Y<Y<Y<Y<Y<Y<Y<Y<Y<Y<E>>>>>>>>>>>;
+}
