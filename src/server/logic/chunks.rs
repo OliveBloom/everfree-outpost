ccb3f0657b9e941f3ef255401cd086f2460e59f8
Stuart Pernsteiner <stuart@pernsteiner.org>
2016-06-30 09:01:33 -0700

    implement the rest of chunks and logic::chunks

--- src/server/logic/chunks.rs
+++ src/server/logic/chunks.rs
@@ -16,22 +16,54 @@ use world::flags;
 use world::object::*;
 
 
+use logic::world::PartialEngine;
+engine_part2!(OnlyChunks(chunks));
+
+
 pub fn get_plane_id(eng: &mut Engine, stable_pid: Stable<PlaneId>) -> PlaneId {
-    unimplemented!()
+    if let Some(pid) = eng.world.transient_plane_id(stable_pid) {
+        trace!("get_plane_id({:?}) = {:?} (hit)", stable_pid, pid);
+        return pid;
+    }
+
+    import_plane(eng, stable_pid)
+        .unwrap_or_else(|e| panic!("import_plane failed: {}", e));
+
+    // import_plane should have created or loaded a Plane with the given StableId.
+    let pid = eng.world.transient_plane_id(stable_pid).unwrap();
+    trace!("get_plane_id({:?}) = {:?} (miss)", stable_pid, pid);
+    pid
 }
 
 // NB: This should only be used when there is reason to believe none of the plane's chunks are
 // loaded.
 pub fn unload_plane(eng: &mut Engine, pid: PlaneId) {
-    unimplemented!();
+    export_plane(eng, pid)
+        .unwrap_or_else(|e| panic!("export_plane failed: {}", e));
 }
 
 pub fn load_chunk(eng: &mut Engine, pid: PlaneId, cpos: V2) {
-    unimplemented!();
+    let (eng, c): (&mut PartialEngine, &mut OnlyChunks) = eng.split();
+
+    c.chunks.load(pid, cpos, |pid, cpos| {
+        import_terrain_chunk(eng, pid, cpos)
+            .unwrap_or_else(|e| panic!("import_terrain_chunk failed: {}", e));
+    });
 }
 
 pub fn unload_chunk(eng: &mut Engine, pid: PlaneId, cpos: V2) {
-    unimplemented!();
+    let unloaded = {
+        let (eng, c): (&mut PartialEngine, &mut OnlyChunks) = eng.split();
+
+        c.chunks.unload(pid, cpos, |pid, cpos| {
+            export_terrain_chunk(eng, pid, cpos)
+                .unwrap_or_else(|e| panic!("export_terrain_chunk failed: {}", e));
+        })
+    };
+
+    if unloaded && eng.chunks.plane_ref_count(pid) == 0 {
+        unload_plane(eng, pid);
+    }
 }
 
 
@@ -69,24 +101,26 @@ fn export_plane(eng: &mut Engine, pid: PlaneId) -> bundle::Result<()> {
 }
 
 
-fn import_terrain_chunk(eng: &mut Engine, pid: PlaneId, cpos: V2) -> bundle::Result<()> {
+fn import_terrain_chunk(eng: &mut PartialEngine, pid: PlaneId, cpos: V2) -> bundle::Result<()> {
     // TODO(plane): use pid + cpos for filename (to avoid requiring a stable_id)
     let opt_tcid = eng.world.plane(pid).get_saved_terrain_chunk_id(cpos);
-    let opt_file = opt_tcid.and_then(|tcid| eng.storage.open_terrain_chunk_file(tcid));
+    let opt_file = opt_tcid.and_then(|tcid| eng.storage().open_terrain_chunk_file(tcid));
     if let Some(mut file) = opt_file {
         trace!("load chunk from file: {:?} @ ({:?}, {:?})", opt_tcid, pid, cpos);
         // TODO: do something intelligent if loading fails, so the whole server doesn't crash
         let b = try!(bundle::read_bundle(&mut file));
         let importer = bundle::import_bundle(&mut DummyFragment::new(&mut eng.world), &b);
-        logic::world::on_import(eng.refine(), &importer);
+        logic::world::on_import(eng, &importer);
     } else {
         trace!("load chunk from terrain_gen: ({:?}, {:?})", pid, cpos);
-        try!(eng.as_ref().as_terrain_gen_fragment().generate(pid, cpos));
+        // FIXME: hacky transmute
+        let eng2: &mut Engine = unsafe { mem::transmute(eng) };
+        try!(eng2.as_ref().as_terrain_gen_fragment().generate(pid, cpos));
     }
     Ok(())
 }
 
-fn export_terrain_chunk(eng: &mut Engine, pid: PlaneId, cpos: V2) -> bundle::Result<()> {
+fn export_terrain_chunk(eng: &mut PartialEngine, pid: PlaneId, cpos: V2) -> bundle::Result<()> {
     // TODO(plane): use pid + cpos for filename
     trace!("unload chunk: ({:?}, {:?})", pid, cpos);
     let stable_tcid = DummyFragment::new(&mut eng.world).plane_mut(pid).save_terrain_chunk(cpos);
@@ -97,19 +131,19 @@ fn export_terrain_chunk(eng: &mut Engine, pid: PlaneId, cpos: V2) -> bundle::Res
         // Don't save chunks that are not fully generated, since they are filled with
         // 'placeholder' block instead of real data.  Instead, let the generated data be
         // discarded, and let the chunk be regenerated the next time it is needed.
-        let mut exporter = bundle::Exporter::new(eng.data);
+        let mut exporter = bundle::Exporter::new(eng.data());
         if !tc.flags().contains(flags::TC_GENERATION_PENDING) {
             exporter.add_terrain_chunk(&tc);
             let b = exporter.finish();
 
-            let mut file = eng.storage.create_terrain_chunk_file(stable_tcid);
+            let mut file = eng.storage().create_terrain_chunk_file(stable_tcid);
             try!(bundle::write_bundle(&mut file, &b));
         }
 
         (tc.id(), exporter)
     };
 
-    logic::world::on_export(eng.refine(), &exporter);
+    logic::world::on_export(eng, &exporter);
     try!(DummyFragment::new(&mut eng.world).destroy_terrain_chunk(tcid));
     Ok(())
 }
