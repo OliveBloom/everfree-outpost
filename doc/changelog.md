f9337460354684ddb8e2007769cc4b27c5f0b7f8
Stuart Pernsteiner <stuart@pernsteiner.org>
2016-07-03 07:33:24 -0700

    match new ui scaling in JS

---
+++ doc/changelog.md
@@ -0,0 +1,5 @@
+# Release [next]
+
+ * Added support for `scale_ui` config setting to the new UI code.  The UI is
+   now rendered at a multiple of the world scale, determined by `scale_ui`.
+
--- src/client/js/resize.js
+++ src/client/js/resize.js
@@ -10,8 +10,14 @@ function calcScale(px) {
 }
 
 function resizeUI(ui_div, w, h) {
-    var scale = Config.scale_ui.get() || calcScale(Math.max(w, h));
-    var factor = scale > 0 ? scale : 1 / -scale;
+    var scale1 = Config.scale_world.get() || calcScale(Math.max(w, h));
+    var factor1 = scale1 > 0 ? scale1 : 1 / -scale1;
+
+    // scale_ui is now applied as a multiple of scale_world.
+    var scale2 = Config.scale_ui.get() || 1;
+    var factor2 = scale2 > 0 ? scale2 : 1 / -scale2;
+
+    var factor = factor1 * factor2;
 
     var virt_w = Math.ceil(w / factor);
     var virt_h = Math.ceil(h / factor);
