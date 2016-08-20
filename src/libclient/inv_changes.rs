4785687196b3022bb94dca3b2a90859a23bf7971
Stuart Pernsteiner <stuart@pernsteiner.org>
2016-08-19 18:14:46 -0700

    fix assertion failure in inv_changes

--- src/libclient/inv_changes.rs
+++ src/libclient/inv_changes.rs
@@ -23,21 +23,19 @@ impl Entry {
 }
 
 
-const BUF_SIZE: usize = 32;
+const BUF_SIZE: usize = 16;
 pub const DISPLAY_TIME: Time = 5000;
 
-// A fixed-size ringbuffer, in which some slots in the "full" part of the buffer may be logically
-// empty.  The buffer can be compacted to free up space.  In addition to the standard `start` and
-// `len` fields, the buffer has an `in_use` count to make it easy to determine whether it's
-// possible to compact.
+// A fixed-size buffer, in which some slots in the "full" part of the buffer may be logically
+// empty.  The buffer can be compacted to free up capacity.  In addition to the standard `len`
+// field, the buffer has an `in_use` count to make it easy to determine whether it's possible to
+// compact.
 pub struct InvChanges {
     buf: [Entry; BUF_SIZE],
-    /// Index of the first slot that's in use.
-    start: usize,
-    /// Offset of one-past-the-last slot from the first.
+    /// Offset of one-past-the-last slot.
     len: usize,
-    /// Number of slots in the range `start .. start + len` that contain nonzero entries.  (Slots
-    /// outside the range always contain zero entries.)
+    /// Number of slots in the range `0 .. len` that contain nonzero entries.  (Slots outside the
+    /// range always contain zero entries.)
     in_use: usize,
 }
 
@@ -45,25 +43,9 @@ impl InvChanges {
     pub fn new() -> InvChanges {
         InvChanges {
             buf: [Entry::empty(); BUF_SIZE],
-            start: 0,
             len: 0,
             in_use: 0,
         }
-    }
-
-    #[inline(always)]
-    fn get(&self, off: usize) -> Entry {
-        self.buf[(self.start + off) % BUF_SIZE]
-    }
-
-    #[inline(always)]
-    fn get_ref(&self, off: usize) -> &Entry {
-        &self.buf[(self.start + off) % BUF_SIZE]
-    }
-
-    #[inline(always)]
-    fn get_mut(&mut self, off: usize) -> &mut Entry {
-        &mut self.buf[(self.start + off) % BUF_SIZE]
     }
 
     fn compact(&mut self) {
@@ -75,8 +57,8 @@ impl InvChanges {
         let mut write = 0;
 
         while read < self.len {
-            if self.get(read).item != 0 {
-                *self.get_mut(write) = self.get(read);
+            if self.buf[read].item != 0 {
+                self.buf[write] = self.buf[read];
                 read += 1;
                 write += 1;
             } else {
@@ -84,7 +66,12 @@ impl InvChanges {
             }
         }
         assert!(write == self.in_use);
-        self.len = write;
+        while write < self.len {
+            self.buf[write] = Entry::empty();
+            write += 1;
+        }
+
+        self.len = self.in_use;
     }
 
     pub fn add(&mut self, now: Time, item: ItemId, count: i16) {
@@ -96,9 +83,11 @@ impl InvChanges {
         let mut count = count;
         for entry in &mut self.buf {
             if entry.item != NO_ITEM && entry.time < now - DISPLAY_TIME {
+                // Entry is expired, clear it out.
                 *entry = Entry::empty();
                 self.in_use -= 1;
             } else if entry.item == item {
+                // Entry isn't expired, but contains the right item.  Merge it into the new entry.
                 count += entry.count;
                 *entry = Entry::empty();
                 self.in_use -= 1;
@@ -111,27 +100,23 @@ impl InvChanges {
 
         // Now add the new entry.
         if self.in_use == BUF_SIZE {
-            // All entries are in use.  Replace the oldest item with this one.
-            let idx = self.len;
-            *self.get_mut(idx) = Entry::new(item, count, now);
-            self.start = (self.start + 1) % BUF_SIZE;
-        } else {
-            // Add the new entry to the end of the list.
-            if self.len == BUF_SIZE {
-                self.compact();
-            }
-            let idx = self.len;
-            *self.get_mut(idx) = Entry::new(item, count, now);
-            self.len += 1;
-            self.in_use += 1;
+            // Buffer is completely full.  Remove the oldest entry.
+            self.buf[0] = Entry::empty();
+            self.in_use -= 1;
         }
+        if self.len == BUF_SIZE {
+            self.compact();
+        }
+
+        self.buf[self.len] = Entry::new(item, count, now);
+        self.len += 1;
+        self.in_use += 1;
     }
 
     pub fn clear(&mut self) {
         for entry in &mut self.buf {
             *entry = Entry::empty();
         }
-        self.start = 0;
         self.len = 0;
         self.in_use = 0;
     }
@@ -158,7 +143,7 @@ impl<'a> Iterator for Iter<'a> {
 
     fn next(&mut self) -> Option<&'a Entry> {
         while self.index < self.owner.len {
-            let entry = self.owner.get_ref(self.index);
+            let entry = &self.owner.buf[self.index];
             self.index += 1;
             if entry.item != NO_ITEM {
                 return Some(entry);
