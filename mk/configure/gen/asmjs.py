40e5fe14ff742b0e4e334e0e22dd0106ad9edb70
Stuart Pernsteiner <stuart@pernsteiner.org>
2017-06-03 16:37:10 +0000

    update asmjs compilation to fix missing functions

--- mk/configure/gen/asmjs.py
+++ mk/configure/gen/asmjs.py
@@ -12,6 +12,7 @@ def rules(i):
             i.rustc_feature_env,
             '$rustc $in',
             '--out-dir $b_asmjs',
+            '--emit link,llvm-ir,dep-info',
             '--cfg asmjs',
             '--cfg \'feature="no_std"\'',
             '--target=$root/util/rustc-target.json',
@@ -23,6 +24,7 @@ def rules(i):
             '-C no-vectorize-loops -C no-vectorize-slp')
 
     return template(r'''
+        em_llvm_link = %{fastcomp('llvm-link')}
         em_llvm_as = %{fastcomp('llvm-as')}
         em_opt = %{fastcomp('opt')}
         em_llc = %{fastcomp('llc')}
@@ -30,18 +32,14 @@ def rules(i):
 
         # See comment in native.py about this sed command
         rule asm_compile_rlib
-            command = %compile_base --emit=link,dep-info $
-                --crate-type=rlib --crate-name=$crate_name $
-                && sed -i -e '\,^$out: ,p;d' $b_asmjs/$crate_name.d
+            command = %compile_base --crate-type=rlib --crate-name=$crate_name $
+                && sed -i -e '\,^$b_asmjs/lib$crate_name.rlib: ,p;d' $b_asmjs/$crate_name.d
             depfile = $b_asmjs/$crate_name.d
             description = RUSTC $out
 
-        rule asm_compile_ir
-            # Like opt-level=3 above, lto is mandatory to prevent emscripten-fastcomp errors.
-            command = %compile_base --emit=llvm-ir,dep-info --crate-type=staticlib -C lto $
-                && sed -i -e '\,^$out: ,p;d' $b_asmjs/$crate_name.d
-            depfile = $b_asmjs/$crate_name.d
-            description = RUSTC $out
+        rule asm_link_ir
+            command = $em_llvm_link -S $in -o $out
+            description = ASMJS $out
 
         rule asm_clean_ir
             command = sed <$in >$out $
@@ -61,6 +59,7 @@ def rules(i):
                 -disable-loop-vectorization -disable-slp-vectorization $
                 -vectorize-loops=false -vectorize-slp=false $
                 -vectorize-slp-aggressive=false $
+                -O3 $
                 -o $out
             description = ASMJS $out
 
@@ -82,21 +81,36 @@ def rules(i):
             description = ASMJS $out
     ''', **locals())
 
+CRATE_DEPS = {}
+
 def rlib(crate_name, deps, src_file=None):
     src_file = src_file or '$root/src/lib%s/lib.rs' % crate_name
 
+    CRATE_DEPS[crate_name] = deps
+
     return template('''
-        build $b_asmjs/lib%{crate_name}.rlib: asm_compile_rlib %src_file $
-            | %for d in deps% $b_asmjs/lib%{d}.rlib %end%
+        build $b_asmjs/lib%{crate_name}.rlib $b_asmjs/%{crate_name}.ll: $
+                asm_compile_rlib %src_file $
+                | %for d in deps% $b_asmjs/lib%{d}.rlib %end%
             crate_name = %crate_name
     ''', **locals())
 
-def asmlibs(name, rust_src, rust_deps, exports_file, template_file):
+def asmlibs(name, exports_file, template_file):
+
+    all_deps = set()
+    def go(x):
+        if x in all_deps:
+            return
+        all_deps.add(x)
+        for d in CRATE_DEPS.get(x, ()):
+            go(d)
+    go(name)
+    all_deps = sorted(all_deps)
+
     return template('''
-        build %base.ll: asm_compile_ir %rust_src $
-            | %for d in rust_deps% $b_asmjs/lib%d.rlib %end%
-            crate_name = %name
-        build %base.clean.ll: asm_clean_ir %base.ll
+        build %base.linked.ll: asm_link_ir $
+                %for d in all_deps% $b_asmjs/%d.ll %end%
+        build %base.clean.ll: asm_clean_ir %base.linked.ll
         build %base.bc: asm_assemble_bc %base.clean.ll
         build %base.exports.txt: asm_convert_exports %exports_file
         build %base.opt.bc: asm_optimize_bc %base.bc | %base.exports.txt
